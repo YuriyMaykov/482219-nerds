e527bc285a03070f0f3c3742200ac16fc723ed77
YuriyMaykov <Yu.A.Maykov@yandex.ru>
2017-12-09 18:03:12 +0500

    Убрал лишний блок в форме, доработал JS

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-var link = document.querySelector(".open-contact-us"); //кнопка Напишите нам
-var popup = document.querySelector(".form-contact"); //секция формы
-var close = popup.querySelector(".close-contact-us"); //кнопка закрытия формы
-var login = popup.querySelector("[name=Your-name]"); //поле с логином
-var e_mail = popup.querySelector("[name=Your-email]"); //поле с почтой
-var leter = popup.querySelector("[name=text-email]"); //текст письма
-var form = popup.querySelector("form"); //форма
+var link = document.querySelector(".open-contact-us");
+var popup = document.querySelector(".form-contact");
+var close = popup.querySelector(".close-contact-us");
+var form = popup.querySelector("form.contact-us");
+var login = popup.querySelector("[name=your-name]");
+var e_mail = popup.querySelector("[name=your-email]");
+var leter = popup.querySelector("[name=text-email]");
 var login_restore = localStorage.getItem("login");
 var email_restore = localStorage.getItem("e_mail");
-
 
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
@@ -23,13 +22,12 @@ link.addEventListener("click", function (evt) {
 
     if(login_restore && email_restore) {
         leter.focus();
-    } 
-    else {
+    }else{
         if(login_restore && !email_restore) {
-        e_mail.focus();
+        	e_mail.focus();
         }
         else{
-        login.focus();
+        	login.focus();
         }
     }
 });
@@ -41,29 +39,6 @@ close.addEventListener("click", function (evt) {
     popup.classList.remove("modal-error");
 });
 
-form.addEventListener("submit", function (evt) {
-    if (!login.value || !e_mail.value || !leter.value) {
-        evt.preventDefault();
-        popup.classList.remove("modal-error");
-        popup.offsetWidth = popup.offsetWidth;
-        popup.classList.add("modal-error");
-        console.log("Заполните все поля пожалуйста !");
-        if (!leter.value){
-            leter.classList.add("invalid");
-        }
-    }
-    else {
-        localStorage.setItem("login", login.value);
-        localStorage.setItem("e_mail", e_mail.value);
-    }
-});
-
-leter.addEventListener("keydown", function (evt) {
-    if (leter.value) {
-        leter.classList.remove("invalid");
-    }
-});
-
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
         if (!popup.classList.contains("visually-hidden")) {
@@ -73,3 +48,66 @@ window.addEventListener("keydown", function (evt) {
         }
     }
 });
+
+form.addEventListener("submit", function (evt) {
+    var another_error=false;
+	var inputs = form.querySelectorAll("input[required]");
+	for(var i = 0; i < inputs.length; i++){
+        if (inputs[i].classList.contains("error")) {
+            another_error=true;
+        }
+    }
+
+    if ((!login.value || !e_mail.value) || another_error==true) {
+        evt.preventDefault();
+        popup.classList.remove("modal-error");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("modal-error");
+    }
+    else {
+        localStorage.setItem("login", login.value);
+        localStorage.setItem("e_mail", e_mail.value);
+    }
+});
+
+var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+if(form != undefined){
+	var inputs = form.querySelectorAll("input[required]");
+	for(var i = 0; i < inputs.length; i++){
+		inputs[i].oninput = function(){
+			//console.log(this)
+			if(this.name == "your-email"){
+				if(this.value.length > 7){
+					if(re.test(this.value)){
+						this.classList.remove("error")
+                    }
+                    else{
+						this.classList.add("error")
+					}
+                }
+                else{
+					if(this.value.length == 0){
+						this.classList.remove("error")
+                    }
+                    else{
+						this.classList.add("error")
+					}
+				}
+            }
+            else{
+				if(this.value.length < 2){
+					if(this.value.length == 0){
+						this.classList.remove("error")
+                    }
+                    else{
+						this.classList.add("error")
+					}
+                }
+                else{
+					this.classList.remove("error")
+				}
+			}
+		}
+	}
+}
+
